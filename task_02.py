dc9ba5136de38496a0d23f55d2aeaf236cf14d65
Maureen Kilmade <maureen.kilmade@spsmail.cuny.edu>
2014-11-06 09:45:52 -0500

    task 2 again using raise

--- task_02.py
+++ task_02.py
@@ -11,8 +11,12 @@ class InvalidAgeError(Exception): pass
 
 def get_age(birthyear):
     age = datetime.datetime.now().year - birthyear
+
     try:
+        if age < 0:
+            raise InvalidAgeError
         if age >= 0:
             return age
     except InvalidAgeError:
-        print "Pick a date in the past."
+        print "No negative numbers"
+        return age
