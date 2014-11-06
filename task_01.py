812a088a230dd587b53b81b448534bd8a183e6a8
Maureen Kilmade <maureen.kilmade@spsmail.cuny.edu>
2014-11-06 14:23:34 -0500

    return, not print

--- task_01.py
+++ task_01.py
@@ -4,9 +4,9 @@
 
 
 def simple_lookup(var1, var2):
-    # return var1[var2]
+    '''put variables into a dict'''
     try:
         return var1[var2]
     except LookupError:
         print "Warning: Your index/key doesn't exist."
-        print var1
+        return var1
