29dcae0f9d969c99ae00255783f3cc6d4beace53
Maureen Kilmade <maureen.kilmade@spsmail.cuny.edu>
2014-11-06 12:19:20 -0500

    instructions confused me

--- task_02.py
+++ task_02.py
@@ -10,13 +10,20 @@ class InvalidAgeError(Exception): pass
 
 
 def get_age(birthyear):
+    '''put in birth year, get an age'''
     age = datetime.datetime.now().year - birthyear
 
-    try:
-        if age < 0:
-            raise InvalidAgeError
-        if age >= 0:
-            return age
-    except InvalidAgeError:
-        print "No negative numbers"
+    if age >= 0:
         return age
+    else:
+        raise InvalidAgeError
+    # try:
+    #     if age < 0:
+    #         raise InvalidAgeError
+    #     elif age >= 0:
+    #         return age
+    # except InvalidAgeError:
+    #     print "Pick a year in the past."
+    #     return age
+print get_age(1999)
+print get_age(2099)
