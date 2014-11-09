98d082b244a8afa95e255d03bb710f1b72566f02
Maureen Kilmade <maureen.kilmade@spsmail.cuny.edu>
2014-11-09 16:34:13 -0500

    current weather, almost there

--- current_weather.py
+++ current_weather.py
@@ -4,20 +4,83 @@
 
 import urllib2
 import json
+import csv
+import os
+from decimal import Decimal
+
 
 base_url = 'http://api.openweathermap.org/data/2.5/weather'
 
+class CurrentWeather(object):
+    '''Gets the weather'''
+    zip_codes = {}
+    base_url = 'http://api.openweathermap.org/data/2.5/weather'
 
-def get_weather(city, country, units='metric'):
-    api_query = '{}?units={}&q={},{}'.format(
-        base_url, units, city, country
-    )
+    def __init__(self, zipcode_data='zipcode_database.csv'):
+        self.read_csv(zipcode_data)
 
-    response = urllib2.urlopen(api_query)
+    def get_weather(self, city, country, units='metric'):
+        api_query = '{}?units={}&q={},{}'.format(
+            base_url, units, city, country
+        )
 
-    return json.load(response)['main']
+        try:
+            response = urllib2.urlopen(api_query)
+        except urllib2.HTTPError, error:
+            raise CurrentWeatherException(
+                error.code,
+                'Error: {} {}'.format(error.code, error.msg))
+
+        return json.load(response)['main']
+
+    # print get_weather('New York', 'us')
+    # print get_weather('San Francisco', 'us')
+    # print get_weather('Austin', 'us')
 
 
-print get_weather('New York', 'us')
-print get_weather('San Francisco', 'us')
-print get_weather('Austin', 'us')
+    def read_csv(self, csv_path):
+        if not os.path.exists(csv_path):
+            raise CurrentWeatherException(
+                9010, 'CSV zipcode database {} not found'.format(csv_path))
+
+        try:
+            inspect = open(csv_path, 'r')
+            report = csv.reader(inspect)
+            for line in report:
+                self.zip_codes[line[0]] = {
+                    'city': line[1],
+                    'state': line[2],
+                    'latitude': line[3],
+                    'longitude': line[4],
+                    'country': line[5]}
+
+        except IOError:
+            raise CurrentWeatherException(
+                4151, 'Error reading {}'.format(csv_path))
+
+        finally:
+            if inspect is not None:
+                inspect.close()
+
+    def get_city_by_zipcode(self, zipcode):
+        '''finds the city name by zip'''
+        try:
+            return self.zip_codes[zipcode]['city']
+        except:
+            raise CurrentWeatherException(
+                5150, 'Error: Zipcode not found in Zipcode data')
+
+
+    def get_weather_by_zipcode(self, zipcode):
+        '''finds the weather based on the zip'''
+        return self.get_weather(
+            self.get_city_by_zipcode, 'US')
+
+
+class CurrentWeatherException(Exception):
+    '''what to expect'''
+
+    def __init__(self, code, message):
+        super(CurrentWeatherException, self).__init__()
+        self.errno = code
+        self.message = message
