1827944e458679c01686326c34f646f534ace56b
Maureen Kilmade <maureen.kilmade@spsmail.cuny.edu>
2014-11-09 17:17:12 -0500

    something wrong with decimals

--- current_weather.py
+++ current_weather.py
@@ -11,6 +11,16 @@ from decimal import Decimal
 
 base_url = 'http://api.openweathermap.org/data/2.5/weather'
 
+
+class CurrentWeatherException(Exception):
+    '''what to expect'''
+
+    def __init__(self, code, message):
+        super(CurrentWeatherException, self).__init__()
+        self.errno = code
+        self.message = message
+
+
 class CurrentWeather(object):
     '''Gets the weather'''
     zip_codes = {}
@@ -20,6 +30,7 @@ class CurrentWeather(object):
         self.read_csv(zipcode_data)
 
     def get_weather(self, city, country, units='metric'):
+        '''finds the weather'''
         api_query = '{}?units={}&q={},{}'.format(
             base_url, units, city, country
         )
@@ -30,15 +41,15 @@ class CurrentWeather(object):
             raise CurrentWeatherException(
                 error.code,
                 'Error: {} {}'.format(error.code, error.msg))
-
-        return json.load(response)['main']
-
+        finally:
+            return json.load(response)['main']
     # print get_weather('New York', 'us')
     # print get_weather('San Francisco', 'us')
     # print get_weather('Austin', 'us')
 
 
     def read_csv(self, csv_path):
+        '''opens a csv'''
         if not os.path.exists(csv_path):
             raise CurrentWeatherException(
                 9010, 'CSV zipcode database {} not found'.format(csv_path))
@@ -47,11 +58,15 @@ class CurrentWeather(object):
             inspect = open(csv_path, 'r')
             report = csv.reader(inspect)
             for line in report:
+                lats = Decimal(line[3])
+                longs = Decimal(line[4])
                 self.zip_codes[line[0]] = {
                     'city': line[1],
                     'state': line[2],
-                    'latitude': line[3],
-                    'longitude': line[4],
+                    # 'latitude': line[3],
+                    # 'longitude': line[4],
+                    'latitude': lats,
+                    'longitude': longs,
                     'country': line[5]}
 
         except IOError:
@@ -70,17 +85,7 @@ class CurrentWeather(object):
             raise CurrentWeatherException(
                 5150, 'Error: Zipcode not found in Zipcode data')
 
-
     def get_weather_by_zipcode(self, zipcode):
         '''finds the weather based on the zip'''
         return self.get_weather(
-            self.get_city_by_zipcode, 'US')
-
-
-class CurrentWeatherException(Exception):
-    '''what to expect'''
-
-    def __init__(self, code, message):
-        super(CurrentWeatherException, self).__init__()
-        self.errno = code
-        self.message = message
+            self.get_city_by_zipcode(zipcode), 'US')
